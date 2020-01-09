0012dea9287a3b5628ba411885beecde1d119dc5
Your Name <your@email.com>
2020-01-09 10:46:29 -0500

    functions

---
+++ function.h
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <errno.h>
+
+int checkletter(char* enter, char* bankletter);
+
+int draw(int lives);
+
+int generate_word();
+
+void setup();
---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+#include <sys/shm.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <errno.h>
+
+
+
+int main(int argc, char *argv[]) {
+    char* input = argv[1];
+    control(input);
+    return 0;
+  }
